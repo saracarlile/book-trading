d1fba99e17bfe93b24f76313410669b8e0a98236
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2017-10-17 12:44:49 -0500

    ported input search function over to my books component and populating modal with content from google books api

--- src/app/all-books/all-books.component.ts
+++ src/app/all-books/all-books.component.ts
@@ -16,48 +16,6 @@ import { Observable } from 'rxjs/Observable';
 export class AllBooksComponent implements OnInit {
 
   books: any[];
-  userinfo = [];
-  search = '';
-  user = new Observable<any>();
-  bookResult = new Observable<any>();
-  bookInfo = '';
-  results = {
-    dbUserInfo: [],
-    gbBookInfo: []
-  }
-  message = '';
-  encodedSearch = '';
- 
-  searchForBookk() {
-    this.bookService.searchForBook().subscribe(result => {   
-      this.userinfo= result,
-      console.log(this.userinfo);
-    });
-  }
-
-  LookupUserInfoAddBook() {
-    this.message = '';
-    if(this.search === ''){
-      this.message = 'You must enter a search term';
-      return;
-    }
-    this.encodedSearch = encodeURIComponent(this.search);
-    this.user = this.bookService.searchForUser();
-    console.log(this.user + " USER INFO");
-    this.bookResult = this.bookService.searchGoogleBooks('https://www.googleapis.com/books/v1/volumes?q=' + this.encodedSearch);
-    console.log(this.bookResult + " BOOK RESULT");
-
-    Observable.forkJoin([this.user, this.bookResult]).subscribe(results => {
-      // results[0] is our character
-      // results[1] is our character homeworld
-      results[0].dbUserInfo = results[0];
-      results[1].gbBookInfo = results[1];
-      console.log(results + " RESULTS");
-      console.log(results[1]["items"]);
-
-    });
-  }
-
 
   constructor(
     private bookService: BooksService) { }
--- src/app/my-books/my-books.component.ts
+++ src/app/my-books/my-books.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { BooksService } from '../books.service';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+
+import 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
+
 
 
 @Component({
@@ -10,9 +15,50 @@ import { BooksService } from '../books.service';
 export class MyBooksComponent implements OnInit {
 
   books: any[];
-  
-    userinfo = [];
-  
+  userinfo = [];
+  search = '';
+  user = new Observable<any>();
+  bookResult = new Observable<any>();
+  bookInfo = '';
+  results = {
+    dbUserInfo: [],
+    gbBookInfo: []
+  }
+  message = '';
+  encodedSearch = '';
+  bookSearch = [];
+ 
+  searchForBookk() {
+    this.bookService.searchForBook().subscribe(result => {   
+      this.userinfo= result,
+      console.log(this.userinfo);
+    });
+  }
+
+  LookupUserInfoAddBook() {
+    this.message = '';
+    if(this.search === ''){
+      this.message = 'You must enter a search term';
+      return;
+    }
+    this.encodedSearch = encodeURIComponent(this.search);
+    this.user = this.bookService.searchForUser();
+    console.log(this.user + " USER INFO");
+    this.bookResult = this.bookService.searchGoogleBooks('https://www.googleapis.com/books/v1/volumes?q=' + this.encodedSearch);
+    console.log(this.bookResult + " BOOK RESULT");
+
+    Observable.forkJoin([this.user, this.bookResult]).subscribe(results => {
+      // results[0] is our character
+      // results[1] is our character homeworld
+      results[0].dbUserInfo = results[0];
+      results[1].gbBookInfo = results[1];
+      console.log(results + " RESULTS");
+      console.log(results[1]["items"]);
+      this.bookSearch = results[1]["items"];
+    });
+  }
+
+
     searchForBook() {
       this.bookService.searchForBook().subscribe(result => {   
         this.userinfo= result,
@@ -23,6 +69,11 @@ export class MyBooksComponent implements OnInit {
     modalStyle = false;
     
     updatemodalStyle() { 
+      this.message = '';
+      if(this.search === ''){
+        this.message = 'You must enter a search term';
+        return;
+      }
       this.modalStyle = !this.modalStyle;
       console.log(this.modalStyle);
     }
